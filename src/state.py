c03afe12918013eaf5995ea37915ceb7d32a11e0
Juha Kivekas <juha.kivekas@wippies.fi>
2015-04-22 16:24:55 +0300

    earlybird: zero throws are now handled correctly

--- src/state.py
+++ src/state.py
@@ -1,42 +1,55 @@
 # balls_in_the_air
-# Juha Kivekäs
+# Juha Kivekas
 # 21.4.2015
 
 class State:
-	"""A class that keeps track of the juggling pattern state"""
+	"""A class that keeps track of the juggling pattern state. Specifically virtual siteswaps."""
 
 	def __init__(self, start=[]):
-		"""initialize a state from a list"""
-		#positive integers and zero indicate landing times/slots for particles
-		#negative integers indicate an unlanded slot in the past
+		"""Initialize a virtual siteswap state from a list"""
+		#positive integers and zero indicate occupied landing times/slots for particles
+		#negative integers indicate unoccupied landing times/slots in the past
 		self.land = start;
 
+	def is_valid_throw(self, t):
+		"""Checks wether the throw t can be thrown without particle collisions"""
+		if t>0 and self.land.count(t) != 0:
+			return False
+		if t<0 and self.land.count(t) == 0:
+			return False
+		if t==0:
+			#zero throws are always allowed?
+			return True
+		return True
+
 	def throw(self, t):
-		"""Do a throw of height 't' on the state"""
-		if self.land.count(0) != 0:
-			#if there is a ball to throw NOW, remove it.
-			self.land.remove(0)
-		else:
-			#if there is no ball to throw NOW, cerate an antiparticle from the PRESENT.
-			self.land.append(0)
-		
-		if t>0:
+		"""Do a throw of height 't' on the state"""		
+		if t>=0:
 			if self.land.count(t) != 0:
 				#if there's a ball landing on time 't' already,
 				#then we will have a collision of particles.
 				print "Throwing ball to existing time slot!"
-	
 			#throw a ball
 			self.land.append(t);
-		if t<0:
+
+		elif t<0:
 			if self.land.count(t) == 0:
 				#if a ball has landed on time 't',
 				#then we will have a collision of antiparticles in the past.
 				print "Throwing ball to existing time slot!"
+			#throw a ball
 			self.land.remove(t)
 
 		#keep the list sorted for clarity
 		self.land.sort();
+
+		if self.land.count(0) != 0:
+			#if there was a ball to throw NOW, remove it.
+			self.land.remove(0)
+		else:
+			#if there was no ball to throw NOW, cerate an antiparticle from the PRESENT.
+			self.land.append(0)
+
 		#progress in time by subtracting one from each state element
 		self.land[:] = [a-1 for a in self.land]
 
