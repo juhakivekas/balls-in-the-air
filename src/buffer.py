c6f82c0ee90264fb8f54622bc003119e92760619
MarkusSallila <markus.sallila@aalto.fi>
2015-05-13 13:01:14 +0300

    added commenting

--- src/buffer.py
+++ src/buffer.py
@@ -6,15 +6,17 @@ class Buffer:
 	def __init__(self, generator):
 		self.throw_generator = generator
 		self.throws = []
+		#creates the empty buffer from Rattern or RandomPattern
 		if (isinstance(generator, Pattern)):
 			for i in xrange(0, -min(generator.siteswap)+1):
 				self.throws.append([])
 		if (isinstance(generator, RandomPattern)):
 			for i in xrange(0, -(generator.min_height)+1):
 				self.throws.append([])
-		
+		#fills the buffer with throws
 		for i in xrange(0, len(self.throws)):
 			throw = self.throw_generator.next_throw()
+			#determines if the throw is normal (>0) or virtual(<0)
 			if (throw > 0):
 				self.throws[i].append(throw)
 			if (throw < 0):
@@ -26,6 +28,7 @@ class Buffer:
 		throw = self.throws.pop(0)
 		self.throws.append([])
 		next = self.throw_generator.next_throw()
+		#determines if the throw is normal (>0) or virtual(<0)
 		if (next > 0):
 			self.throws[-1].append(next)
 		if (next < 0):
