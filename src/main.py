7f0bb90972910a5a4f5fcaa54d4fd50c4d678683
Otto Tuominen <otto.tuominen@aalto.fi>
2015-05-17 17:07:38 +0300

    simulation can now be started from command line using parameters (usage information in the comments)

--- src/main.py
+++ src/main.py
@@ -3,31 +3,56 @@ from pattern import *
 from random_pattern import *
 from state import *
 
-#----CURRENT WORKING CONDITION
-"""
-#create a cyclic pattern
-p = Pattern([4,2,3]); 
-sim = Simulation(p);
-sim.run()
+import argparse
+
 """
 
-#----goal for week2
-#"""
-#start from the three ball ground state
-#create a random pattern starting from the state 's'
-r = RandomPattern(3,-2,6)
-sim = Simulation(r);
-sim.run()
-#"""
+--b is the number of balls
+--r for random pattern
+--dist \"g\" means that geometric distribution is going to be used, other
+values are ignored and default value for the distribution is used
+--min minimum throw height (if it is < 0 then virtual throws are allowed)
+--max maxmimum throw height
+--p user specified pattern
 
-#---goal for week2/3?
+examples:
+
+python2 main.py --p 1 2 3 4 5
+starts simulation with user specified pattern
+
+python2 main.py --r --max 8 --min -2 --b 3 --dist "g"
+starts random pattern simulation with 3 balls, maximum throw height of 8,
+minimum throw height of -2 and using geometric distribution
+
 """
-#start from the three ball ground state
-s = State([0,1,2])
-#create a random pattern starting from the state 's'
-r = RandomPattern(s)
-#create a buffer for the random pattern
-b = Buffer(r)
-sim = Simulation(r);
-sim.run()
-"""
+
+
+parser = argparse.ArgumentParser(description="Juggling simulator")
+parser.add_argument('--b', action="store", dest="num_balls", type=int)
+parser.add_argument('--r', action="store_true", dest="enable_random")
+parser.add_argument('--dist', action="store", dest="distribution")
+parser.add_argument('--min', action="store", dest="min_throw", type=int)
+parser.add_argument('--max', action="store", dest="max_throw", type=int)
+parser.add_argument('--p', action="store", nargs="+", dest="pattern", type=int)
+parser.set_defaults(enable_random=False)
+args = parser.parse_args()
+
+print(args.enable_random)
+
+if args.enable_random:
+    dist = "u"
+    if args.distribution == "g":
+        dist = "g"
+    elif args.distribution != "None":
+        print("Invalid distribution type!")
+        print("Using default value (uniform) for the distribution!")
+
+    random_pattern = RandomPattern(args.num_balls, args.min_throw, args.max_throw, dist_type=dist)
+    buffer = Buffer(random_pattern)
+    simulation = Simulation(buffer)
+    simulation.run()
+else:
+    s = Pattern(args.pattern)
+    buffer = Buffer(s)
+    simulation = Simulation(buffer)
+    simulation.run()
